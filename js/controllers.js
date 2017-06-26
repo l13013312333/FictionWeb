a1027b0a19e5d3b4908bdd65ea6521b1d34ad534
l13013312333 <l13013312333@gmail.com>
2017-06-26 23:50:38 +0800

    nothingRight

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,10 @@
 function TodoCtrl($scope){
     $scope.newItem="";
-    $scope.todoList = [{label: "新事項"}, {label: "阿明"}];
+    $scope.todoList = [];
 
     $scope.addItem = function(){
         if(this.newItem){
-            THIS.todoList.push({label: this.newItem});
+            THIS.todoList.push({label:this.newItem});
             this.newItem = "";
         }
     }
