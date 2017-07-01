2ef6fe828ceafd338b2cb84da2f21006324746fd
l13013312333 <l13013312333@gmail.com>
2017-07-01 16:48:15 +0800

    div

--- js/index.js
+++ js/index.js
@@ -1,21 +1,16 @@
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-     // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-};
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-    console.log('User signed out.');
-    });
+document.getElementById("Gsign").style.visibility="hidden";
+var bar = 0;
+count();
+function count(){
+    bar = bar + 4;
+    document.getElementById("progressbar").style.width = bar+"%";
+    if(bar < 99){
+        setTimeout("count()", 100);
+    }
+    else{
+        //正式進入網站內容
+        document.getElementById("Gsign").style.visibility="visible";
+        document.getElementById("progress").style.visibility="hidden";
+        document.getElementById("progressbar").style.visibility="hidden";
+    }
 }
